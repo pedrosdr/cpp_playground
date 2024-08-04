2dab7b00d22c3851bcd5a5890c3296ba67199a4a
Pedro Sartori <pedrosartdr@gmail.com>
2024-08-04 15:23:33 -0300

    Lambda Expressions

--- lambda.cpp
+++ lambda.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <functional>
+#include <cmath>
 
 void invoke(int a, int b, std::function<void(int,int)> func) 
 {
     func(a, b);
+}
+
+float apply(float f, std::function<float(float)> func)
+{
+    return func(f);
+}
+
+void sub(int a, int b) 
+{
+    std::cout << a - b;
 }
 
 int main()
@@ -14,5 +25,9 @@ int main()
             std::cout << a+b;
         }
     );
+
+    invoke(5,3, sub);
+
+    std::cout << "\n" << apply(5.0, exp); 
     return 0;
 }
